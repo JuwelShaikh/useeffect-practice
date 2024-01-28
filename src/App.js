9cd40f1bd68f5e7c42332b05f4f348806b2cafd3
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-28 16:28:07 +0600

    Data Showed

--- src/App.js
+++ src/App.js
@@ -21,9 +21,29 @@ const Comments = () =>{
 
   return(
     <div>
-      <h2>Id:</h2>
-      <h1>Name: </h1>
-      <p>Comments:{comments.length}</p>
+      <h2>Total comment: {comments.length}</h2>
+      {
+        comments.map(comment => <Comment name = {comment.name} id= {comment.id} body={comment.body} /> )
+      }
+    </div>
+  )
+}
+
+// style
+const styleProps = {
+  backgroundColor: 'skyblue',
+  margin: '5px',
+  padding: '5px',
+  border: '3px solid black',
+  borderRadius: '10px'
+}
+
+const Comment = (props) =>{
+  return(
+    <div style={styleProps}>
+      <h2>Name: {props.name}</h2>
+      <h3>ID: {props.id}</h3>
+      <p>{props.body}</p>
     </div>
   )
 }
