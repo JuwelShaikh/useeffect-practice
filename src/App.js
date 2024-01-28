a7f502af4514de9b9b4555e3d7418aaba50fae9e
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-28 12:59:05 +0600

    Component added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <Comments/>
     </div>
   );
+}
+
+const Comments = () =>{
+  return(
+    <div>
+      <h2>Id:</h2>
+      <h1>Name: </h1>
+      <p>Comments:</p>
+    </div>
+  )
 }
 
 export default App;
